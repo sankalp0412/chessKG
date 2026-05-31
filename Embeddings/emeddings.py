ac40fee345d4f0bb4d1dfd73b670346eef7de16b
sankalp0412 <sankalpdhupar@Sankalps-MacBook-Air.local>
2026-05-31 22:37:40 +0200

    Finished Ontology

---
+++ Embeddings/emeddings.py
@@ -0,0 +1,13 @@
+from rdfpandas.graph import to_dataframe
+import pandas as pd
+import rdflib
+from pathlib import Path
+
+SCRIPT_DIR = Path(__file__).resolve().parent
+PROJECT_ROOT = SCRIPT_DIR.parent
+
+# g = rdflib.Graph()
+
+# g.parse(
+#     "/../TTl Files Graph/TTl Files Graph/All_Ontology_and_SHACL.trig", format="trig"
+# )
